3b5860971d015b06fbf8a8e86416f1964625a301
Arabprogrammer <Arabprogrammer3030@gmail.com>
2021-05-30 16:05:01 +0200

    hibernate validation for User

--- src/main/java/com/app/controller/UserController.java
+++ src/main/java/com/app/controller/UserController.java
@@ -3,22 +3,20 @@ package com.app.controller;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
-import org.apache.commons.lang3.StringUtils;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.app.model.Role;
 import com.app.model.User;
@@ -33,7 +31,6 @@ public class UserController {
 	@Autowired
 	RoleRepo roleRepo;
 
-	public static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^(.+)@(.+)$", Pattern.CASE_INSENSITIVE);
 
 	@GetMapping({ "/", "/signIn" })
 	public String login() {
@@ -41,7 +38,8 @@ public class UserController {
 	}
 
 	@GetMapping("/signUp")
-	public String signUp() {
+	public String signUp(Model model) {
+		model.addAttribute("user", new User());
 		return "registration";
 	}
 
@@ -50,67 +48,49 @@ public class UserController {
 		return "home";
 	}
 
+//	required
+//	minlength="8"
+	/*
+	 * The BindingResult must come right after the model object that is validated or
+	 * else Spring will fail to validate the object and throw an exception.
+	 */
 	@PostMapping(path = "/registration")
-	public String createNewUser(@RequestParam Map<String, String> requestBody, Model model) {
-		List<String> errors = getErrors(requestBody);
+	public String createNewUser(@Valid User user, BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) {
+			return "registration";
+		}
+		List<String> errors = checkIfPasswordAndConfirmPasswordValid(user);
 		if (!errors.isEmpty()) {
 			model.addAttribute("errors", errors);
 			return "registration";
 		}
-		Optional<User> userExists = userRepo.findByEmail(requestBody.get("email"));
+		Optional<User> userExists = userRepo.findByEmail(user.getEmail());
 		if (userExists.isPresent()) {
-			errors.add(requestBody.get("email") + " is already being used");
+			errors.add(user.getEmail() + " is already being used");
 			model.addAttribute("errors", errors);
 			return "registration";
 		} else {
-			String firstName = requestBody.get("firstName");
-			String lastName = requestBody.get("lastName");
-			String email = requestBody.get("email");
-			String password = requestBody.get("password");
-			String birthday = requestBody.get("birthday");
+			String firstName = user.getFirstName();
+			String lastName = user.getLastName();
+			String email = user.getEmail();
+			String password = user.getPassword();
+			LocalDate birthday = user.getBirthday();
 			Set<Role> roles = roleRepo.findByRole("USER").stream().collect(Collectors.toSet());
-			User user = new User(firstName, lastName, LocalDate.parse(birthday), email,
-					new BCryptPasswordEncoder().encode(password), false, true, roles);
-			userRepo.save(user);
+			User toSave = new User(firstName, lastName, birthday, email, new BCryptPasswordEncoder().encode(password),
+					false, true, roles);
+			userRepo.save(toSave);
 			return "login";
 		}
 	}
 
-	private List<String> getErrors(Map<String, String> requestBody) {
+	private List<String> checkIfPasswordAndConfirmPasswordValid(User user) {
 		List<String> errors = new ArrayList<>();
-		String firstName = requestBody.get("firstName");
-		if (StringUtils.trimToEmpty(firstName).isEmpty() || firstName.length() < 2) {
-			errors.add("First name is not valid.");
-		}
-		String lastName = requestBody.get("lastName");
-		if (StringUtils.trimToEmpty(lastName).isEmpty() || lastName.length() < 2) {
-			errors.add("Last name is not valid.");
-		}
-		String email = requestBody.get("email");
-		if (StringUtils.trimToEmpty(email).isEmpty() || !isEmailValid(email)) {
-			errors.add("Email is not valid.");
-		}
-		String password = requestBody.get("password");
-		if (StringUtils.trimToEmpty(password).isEmpty()) {
-			errors.add("Password is not valid.");
-		}
-		String repeatPassword = requestBody.get("repeatPassword");
-		if (StringUtils.trimToEmpty(repeatPassword).isEmpty()) {
-			errors.add("Confirm password is not valid.");
-		}
+		String password = user.getPassword();
+		String repeatPassword = user.getConfirmPassword();
 		if (!Objects.equals(repeatPassword, password)) {
 			errors.add("Passwords Don't Match.");
 		}
-		String birthday = requestBody.get("birthday");
-		if (StringUtils.trimToEmpty(birthday).isEmpty()) {
-			errors.add("Birthday is not valid.");
-		}
 		return errors;
-	}
-
-	public static boolean isEmailValid(String email) {
-		Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
-		return matcher.matches();
 	}
 
 }
--- src/main/java/com/app/model/User.java
+++ src/main/java/com/app/model/User.java
@@ -13,10 +13,16 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
-
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name = "user")
@@ -27,18 +33,34 @@ public class User {
 	private Integer id;
 
 	@Column(name = "firstname")
+	@NotNull(message = "FirstName cannot be null")
+	@Size(min = 2, max = 20, message = "Firstname must be between 2 and 20 characters")
 	private String firstName;
 
-	@Column(name = "lasttname")
+	@Column(name = "lastname")
+	@NotNull(message = "LastName cannot be null")
+	@Size(min = 2, max = 20, message = "LastName must be between 2 and 20 characters")
 	private String lastName;
 
 	@Column(name = "birthday", columnDefinition = "DATE")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@NotNull(message = "Birthday cannot be null")
+	@Past(message = "Birthday must be a past date")
 	private LocalDate birthday;
 
 	@Column(name = "email", unique = true)
+	@NotEmpty(message = "Email cannot be null")
+	@Email(message = "Email should be valid")
 	private String email;
 
+	@Transient
+	@NotNull(message = "Confirm Password cannot be null")
+	@Size(min = 8,max = 20, message = "Confirm Password must be between 8 and 16 characters")
+	private String confirmPassword;
+
 	@Column(name = "password")
+	@NotNull(message = "Password cannot be null")
+	@Size(min = 8,max = 20, message = "Password must be between 8 and 16 characters")
 	private String password;
 
 	@Column(name = "is_locked")
@@ -84,16 +106,13 @@ public class User {
 		return password;
 	}
 
-
 	public String getFirstName() {
 		return firstName;
 	}
 
-
 	public String getLastName() {
 		return lastName;
 	}
-
 
 	public boolean isAccountLocked() {
 		return isLocked;
@@ -107,10 +126,38 @@ public class User {
 		return birthday;
 	}
 
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setBirthday(LocalDate birthday) {
+		this.birthday = birthday;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", birthday=" + birthday
-				+ ", email=" + email + ", password=" + password + ", isLocked=" + isLocked + ", isEnabled=" + isEnabled
-				+ ", roles=" + roles + "]";
+				+ ", email=" + email + ", confirmPassword=" + confirmPassword + ", password=" + password + ", isLocked="
+				+ isLocked + ", isEnabled=" + isEnabled + ", roles=" + roles + "]";
 	}
 }
